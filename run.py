0cdc80ea0bdff8de74f9d7190d7149c64fe8a051
Michelle Yunorvi Luna Alama <miluna@Michelles-MacBook-Pro.local>
2023-12-06 12:23:32 -0500

    Pruebas desde MAC, modificado el bash

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ load_dotenv()
 guardar_agentes_cloud()
 
 def abrir_navegador():
-    webbrowser.open_new('http://localhost:5000/')
+    webbrowser.open_new('http://localhost:4999/')
 
 if __name__ == '__main__':
     Timer(1.5, abrir_navegador).start()  # Espera 1.5 segundos para abrir el navegador
-    app.run(debug=True)
+    app.run(debug=True, port=4999)
